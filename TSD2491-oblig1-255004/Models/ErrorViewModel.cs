b96408b05c47b44cae3462d391ba26ba6a37899a
Martin-Rolando <255004@usn.no>
2024-04-19 22:56:30 +0200

    Commit1
    Oppsett av prosjekt i Visual Studio Community 2022.

---
+++ TSD2491-oblig1-255004/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TSD2491_oblig1_255004.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
